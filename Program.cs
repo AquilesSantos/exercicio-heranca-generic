73f15815aba79f842c37ae42c4f4b9e7cb572ae2
Aquiles Silva dos Santos <asantos@marlin.net>
2021-08-28 13:29:32 -0300

    :rocket: Exercício herença e generic

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Heranca
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var conta = new ContaCorrente(20);
+
+      var contaCorrente = new ContaCorrente(20);
+      var contaInvestimento = new ContaInvestimento();
+
+      var convert = ConverteConta(contaCorrente, contaInvestimento);
+      Console.WriteLine($"Saldo: {convert.Saldo}");
+      Console.WriteLine($"TipoConta: {convert.GetType().Name}");
+
+    }
+    public static C2 ConverteConta<C1, C2>(C1 conta1, C2 conta2) where C1 : Conta where C2 : Conta
+    {
+      //   var a = Activator.CreateInstance<A>();
+      //   var b = Activator.CreateInstance<B>();
+
+      conta2.setAttributes(conta1.Agencia, conta1.Numero, conta1.Saldo);
+      return conta2;
+    }
+  }
+}
